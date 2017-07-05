975527097bc5ab48cfd3918a6038c056ab46ad02
Clement CAZUC <ccazuc@e3r12p15.42.fr>
2017-07-05 16:49:51 +0200

    Comment massacrer un fichier

--- j02/ex05/ft_print_comb2_save.c
+++ j02/ex05/ft_print_comb2_save.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 10:49:02 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/05 16:49:04 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/05 15:36:32 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,47 +35,45 @@ void	ft_print_value(char i, char j, char k, char l)
 	}
 }
 
-void	ft_loop(char *table, char *bol)
-{
-	while (table[2] <= '9')
-	{
-		table[3] = '0';
-		if (*bol == '1')
-		{
-			*bol = '0';
-			table[3] = table[1] + 1;
-			table[2] = table[0];
-		}
-		while (table[3] <= '9')
-		{
-			ft_print_value(table[0], table[1], table[2], table[3]);
-			table[3]++;
-		}
-		table[2]++;
-	}
-}
-
 void	ft_print_comb2(void)
 {
+	char i;
+	char j;
+	char k;
+	char l;
 	char bol;
-	char table[4];
 
-	bol = '0';
-	table[0] = '0';
-	table[1] = '0';
-	table[2] = '0';
-	table[3] = '1';
-	while (table[0] <= '9')
+	i = '0';
+	j = '0';
+	k = '0';
+	l = '1';
+	while (i <= '9')
 	{
-		table[1] = '0';
-		while (table[1] <= '8')
+		j = '0';
+		while (j <= '8')
 		{
-			table[2] = table[0];
-			ft_loop(table, &bol);
-			bol = '1';
-			table[1]++;
+			k = '0';
+			k = i;
+			while (k <= '9')
+			{
+				l = '0';
+				if (bol == 1)
+				{
+					bol = 0;
+					l = j + 1;
+					k = i;
+				}
+				while (l <= '9')
+				{
+					ft_print_value(i, j, k, l);
+					l++;
+				}
+				k++;
+			}
+			bol = 1;
+			j++;
 		}
-		table[0]++;
+		i++;
 	}
 }
 
