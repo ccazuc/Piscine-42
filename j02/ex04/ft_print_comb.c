305ac3083bd2618518a029a6678ffeb416977755
Clement CAZUC <ccazuc@e3r12p15.42.fr>
2017-07-05 10:48:19 +0200

    Added ex04

---
+++ j02/ex04/ft_print_comb.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+int		ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void ft_is_negative(int n)
+{
+	char i;
+	char j;
+	char k;
+
+	i = '0';
+	j = '1';
+	k = '2';
+	while(i <= '7')
+	{
+		j = i+1;
+		while(j <= '8')
+		{
+			k = j+1;
+			while(k <= '9')
+			{
+				ft_putchar(i);
+				ft_putchar(j);
+				ft_putchar(k);
+				if(!(i == '7' && j == '8' && k == '9'))
+				{	
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				k++;	
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int 	main()
+{
+	ft_is_negative(-1);
+	return (0);
+}
+	
