8fab84dd8fd8f205dd1c27ae10ff114c008ebdf9
Clement CAZUC <ccazuc@e3r12p16.42.fr>
2017-07-06 11:05:47 +0200

    Added j03

---
+++ j03/ex03/ft_div_mod.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/06 11:01:02 by ccazuc            #+#    #+#             */
+/*   Updated: 2017/07/06 11:05:35 by ccazuc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	if (b == 0)
+		return;
+	*div = a / b;
+	*mod = a % b;
+}
+
+int		main(void)
+{
+	int		a;
+	int		b;
+	int		div;
+	int		mod;
+
+	a = 5;
+	b = 2;
+	ft_div_mod(a, b, &div, &mod);
+	printf("div: %d, mob: %d", div, mod);
+	return (0);
+}
