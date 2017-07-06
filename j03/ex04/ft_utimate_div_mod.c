b4127eb6cc42e9b6a29872efb5cf5f1f23704be8
Clement CAZUC <ccazuc@e3r12p16.42.fr>
2017-07-06 11:10:50 +0200

    Added ex04

---
+++ j03/ex04/ft_utimate_div_mod.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utimate_div_mod.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/06 11:06:27 by ccazuc            #+#    #+#             */
+/*   Updated: 2017/07/06 11:10:37 by ccazuc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	if(*b == 0)
+		return;
+	int		tmp_div;
+	int		tmp_mod;
+
+	tmp_div = *a / *b;
+	tmp_mod = *a % *b;
+	*a = tmp_div;
+	*b = tmp_mod;
+}
+
+int		main(void)
+{
+	int		a;
+	int		b;
+
+	a = 5;
+	b = 2;
+	ft_ultimate_div_mod(&a, &b);
+	printf("a: %d, b: %d", a, b);
+	return (0);
+}
