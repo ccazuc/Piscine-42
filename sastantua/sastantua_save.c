2974c74a06d691af946a4e75f86554d1ef1f3399
Clement CAZUC <ccazuc@e3r11p15.42.fr>
2017-07-08 21:58:44 +0200

    Finished sastantua

--- sastantua/sastantua_save.c
+++ sastantua/sastantua_save.c
@@ -6,10 +6,11 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 13:19:34 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/08 21:58:14 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/08 21:46:45 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -34,13 +35,15 @@ int		get_total_length(int size)
 		stage_line = -1;
 		while (++stage_line < stage_height)
 		{
-			line_len += 2;
+				line_len += 2;
+		//		printf("stage: %d, line: %d, line_len: %d\n", stage_nb, stage_line, line_len);
 		}
 		if (stage_nb % 2 == 0 && stage_nb >= 2)
-			stage_len_offset += 2;
+			 stage_len_offset += 2;
 		if (stage_nb < size - 1)
 			line_len += stage_len_offset;
 		stage_height++;
+		//printf("line_len : %d, stage_nc: %d, stage_len: %d\n", line_len, stage_nb, stage_len_offset);
 	}
 	return (line_len - 2);
 }
@@ -85,7 +88,7 @@ void	draw_door_line(int tot_len, int *line_len, int size, int cur_line)
 		else if (stag_ln >= tot_len / 2 - door_s / 2 && stag_ln <= tot_len / 2
 				+ door_s / 2)
 			ft_putchar('|');
-		else if (stag_ln >= tot_len / 2 - *line_len / 2 && stag_ln <=
+		else if (stag_ln >= tot_len / 2 - *line_len / 2 && stag_ln <= 
 				tot_len / 2 - *line_len / 2 + *line_len)
 			ft_putchar('*');
 		else if (stag_ln < tot_len / 2 - *line_len / 2)
@@ -105,41 +108,53 @@ int		draw_door(int total_len, int line_len, int size)
 			draw_door_line(total_len, &line_len, size, current_line);
 		else
 			draw_line(total_len, &line_len);
+//		printf("door, len: %d, line_len: %d\n", current_line, line_len);
 		line_len += 3;
 		ft_putchar('\n');
 	}
+//	printf("door: %d\n", line_len);
 	return (0);
 }
 
 void	sastantua(int size)
 {
-	int		table[6];
+	int		stage_nb;
+	int		line_len;
+	int		stage_line;
+	int		stage_height;
+	int		total_len;
+	int		stag_len_off;
 
-	table[0] = -1;
-	table[1] = 3;
-	table[3] = 3;
-	table[4] = get_total_length(size);
-	table[5] = 4;
-	while (++table[0] < size)
+	total_len = get_total_length(size);
+	stage_nb = -1;
+	stage_height = 3;
+	line_len = 3;
+	stag_len_off = 4;
+	while (++stage_nb < size)
 	{
-		if (table[0] == size - 1 && !draw_door(table[4], table[1], size))
+		if (stage_nb == size - 1 && !draw_door(total_len, line_len, size))
 			return ;
-		table[2] = -1;
-		while (++table[2] < table[3])
+		stage_line = -1;
+		while (++stage_line < stage_height)
 		{
-			draw_line(table[4], &table[1]);
-			table[1] += 3;
+			draw_line(total_len, &line_len);
+			line_len += 3;
 			ft_putchar('\n');
+//			printf("stage: %d, line: %d, line_len: %d\n", stage_nb, stage_line, line_len);
 		}
-		if (table[0] % 2 == 0 && table[0] >= 2)
-			table[5] += 2;
-		table[1] += table[5];
-		table[3]++;
+		if (stage_nb % 2 == 0 && stage_nb >= 2)
+			stag_len_off += 2;
+		line_len += stag_len_off;
+		stage_height++;
+	//	printf("line_len:  %d, stage_nb: %d, stag_of: %d\n", line_len, stage_nb, stag_len_off);
 	}
 }
 
 int		main(void)
-{	
-	sastantua(6);
+{
+	int		size;
+
+	size = 7;
+	sastantua(size);
 	return (0);
 }
