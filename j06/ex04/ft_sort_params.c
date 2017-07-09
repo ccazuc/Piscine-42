0a5160812d8bcd7a5d1164b64a4c9a413afe7a0f
Clement CAZUC <ccazuc@e3r12p16.42.fr>
2017-07-09 16:59:59 +0200

    Update

--- j06/ex04/ft_sort_params.c
+++ j06/ex04/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/09 16:08:35 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/09 16:44:07 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/09 16:55:07 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ void	sort_table(char **str)
 		while (str[++j][0])
 			if (str[j][0] > str[i][0])
 			{
-				*tmp_str = str[j][0];
-				str[j][0] = str[i][0];
-				str[i][0] = *tmp_str;
+				tmp_str = str[j];
+				str[j] = str[i];
+				str[i] = tmp_str;
 			}
 	}
 }
@@ -45,23 +45,21 @@ void	sort_table(char **str)
 int		main(int argc, char **argv)
 {
 	int		i;
+	int		j;
 	char	*tab[argc - 1];
 	
-	//ft_putstr(argv[0]);
-	//ft_putstr(argv[1]);
 	i = argc;
-	while (--i > 0)
-		tab[i][0] = argv[i][0];
-	//sort_table(tab);
+	while (--i	> 0)
+	{
+			tab[i] = argv[i];
+	}
+	sort_table(tab);
 	tab[1][0] = argv[1][0];
-	i = -1;
-	ft_putstr(&argv[1][0]);
-	//ft_putstr(&tab[1]);
-	ft_putstr(&tab[1][0]);
+	i = 0;
 	while (++i < argc)
 	{
-		//ft_putstr(&tab[i]);
-		//ft_putchar('\n');
+		ft_putstr(tab[i]);
+		ft_putchar('\n');
 	}
 	return (0);
 }
