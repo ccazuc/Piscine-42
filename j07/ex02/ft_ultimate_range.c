e195b0a24ba70bf3f1d3c3214f86b1202864a301
Clement CAZUC <ccazuc@e3r12p16.42.fr>
2017-07-09 21:00:35 +0200

    Update

--- j07/ex02/ft_ultimate_range.c
+++ j07/ex02/ft_ultimate_range.c
@@ -1,44 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/09 20:25:07 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/09 20:55:48 by ccazuc           ###   ########.fr       */
+/*   Created: 2017/07/09 20:51:04 by ccazuc            #+#    #+#             */
+/*   Updated: 2017/07/09 20:59:46 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int		*ft_range(int min, int max)
+int	 ft_ultimate_range(int **range, int min, int max)
 {
 	int		*tab;
 	int		i;
 
 	if (min >= max)
-		return tab;
+	{
+		range = NULL;
+		return (0);
+	}
 	tab = malloc((max - min) * 4);
-	i = 0;
+	i = -1;
 	while (min < max)
 	{
 		tab[i] = min;
 		++i;
 		++min;
 	}
-	return tab;
+	*range = tab;
+	return i;
 }
 
 int		main(void)
 {
-	int		*tab;
+	int		**tab;
 	int		i;
+	int		size;
 
-	tab = ft_range(-10, -5);
+	size = ft_ultimate_range(tab, 0, 10);
 	i = -1;
-	while (++i < 5)
-		printf("i: %d, nb: %d\n", i, tab[i]);
+	printf("size: %d\n", size);
+	while (++i < 10)
+		printf("i: %d, nb: %d\n", i, *tab[i]);
 	return (0);
 }
