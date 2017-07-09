08e1cd2049fb60c53c57a277bbe41077dd8ba4fc
Clement CAZUC <ccazuc@e3r12p16.42.fr>
2017-07-09 16:06:40 +0200

    Added ex03

--- j06/ex03/ft_rev_params.c
+++ j06/ex03/ft_rev_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/09 15:50:03 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/09 16:02:56 by ccazuc           ###   ########.fr       */
+/*   Created: 2017/07/09 15:59:31 by ccazuc            #+#    #+#             */
+/*   Updated: 2017/07/09 16:05:42 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	ft_putstr(char *str)
 
 	i = -1;
 	while (str[++i])
-		ft_putchar(str[i]);
+		ft_putchar(str[i]);		
 }
 
 int		main(int argc, char **argv)
 {
 	int		i;
 
-	i = 0;
-	while (argv[++i])
+	i = argc;
+	while (--i > 0)
 	{
 		ft_putstr(argv[i]);
 		ft_putchar('\n');
