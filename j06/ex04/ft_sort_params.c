cf8ae30cf4432f50c2078d5ac6ad81f177e8b677
Clement CAZUC <ccazuc@e3r12p16.42.fr>
2017-07-09 17:22:21 +0200

    Updte

--- j06/ex04/ft_sort_params.c
+++ j06/ex04/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/09 16:08:35 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/09 16:55:07 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/09 17:20:58 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,40 @@ void	ft_putstr(char *str)
 		ft_putchar(str[i]);
 }
 
-void	sort_table(char **str)
+int		ft_strcmp(char *s1, char *s2)
+{
+	int		i;
+
+	i = - 1;
+	while (s1[++i] && s2[i])
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+	if (s2[i])
+		return (- s2[i]);
+	if (s1[i])
+		return (s1[i]);
+	return (0);
+}
+
+void	sort_table(char **str, int len)
 {
 	int		i;
 	int		j;
-	char	*tmp_str;
+	char	*tmp[len];
 
 	i = -1;
 	j = -1;
-	while (str[++i][0])
+	while (str[++i])
 	{
 		j = -1;
-		while (str[++j][0])
-			if (str[j][0] > str[i][0])
+		while (str[++j])
+			if (ft_strcmp(str[j], str[i]) > 0)
 			{
-				tmp_str = str[j];
+				tmp[j] = str[j];
 				str[j] = str[i];
-				str[i] = tmp_str;
+				str[i] = tmp[j];
 			}
 	}
 }
@@ -53,9 +70,9 @@ int		main(int argc, char **argv)
 	{
 			tab[i] = argv[i];
 	}
-	sort_table(tab);
-	tab[1][0] = argv[1][0];
-	i = 0;
+	sort_table(tab, argc - 1);
+	//tab[1][0] = argv[1][0];
+	i = -1;
 	while (++i < argc)
 	{
 		ft_putstr(tab[i]);
