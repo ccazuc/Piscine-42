03f297c03e009638e15b1b8b4beb495cccfbb72e
Clement CAZUC <ccazuc@e3r12p14.42.fr>
2017-07-11 11:55:21 +0200

    J07/ex04 update

--- j07/ex04/ft_split_whitespaces.c
+++ j07/ex04/ft_split_whitespaces.c
@@ -6,79 +6,57 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/10 16:15:48 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/10 17:35:52 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/11 11:54:24 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-char	*remove_useless_char(char *str, int *len, char **result)
+int		get_nb_word(char *str)
 {
 	int		i;
-	int		res_i;
-	char	is_corr_last;
+	int		nb_word;
+	int		last_word;
+	char	tmp;
 
-	is_corr_last = 0;
+
 	i = -1;
-	res_i = 0;
-	result = malloc(*len);
+	last_word = 0;
+	nb_word = str[0] == (str[0] == ' ') ? 0 : 1;
 	while (str[++i])
 	{
-		if ((str[i] == ' ' || str[i] == '\n' || str[i] == 9) && !is_corr_last)
-		{
-			is_corr_last = 0;
+		tmp = str[i];
+		if ((tmp  == ' ' || tmp == '\n' || tmp == 9) && !last_word)
 			continue ;
+		if ((tmp == ' ' || tmp == '\n' || tmp == 9) && last_word)
+		{
+			nb_word++;
+			last_word = 0;
 		}
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == 9)
-			is_corr_last = 1;
-		result[res_i] = str[i];
-		res_i++;
+		else
+			last_word = 1;
 	}
-	*len = res_i - 1;
-	return (result);
+	return (nb_word);
 }
 
-char	**ft_split_whitespaces(char *str, char **result)
+char	**ft_split_whitespaces(char *str)
 {
 	int		i;
 	int		res_i;
 	int		res_j;
 	int		*tmp;
+	char	**result;
 
-	i = 0;
-	tmp = &i;
-	while (str[i])
-		++i;
-	tmp = remove_useless_char(str, i);
-	result = malloc(i * sizeof(**result));
-	i = -1;
-	res_i = 0;
-	res_j = 0;
-	while (tmp[++i])
-	{
-		result[res_j][res_i] = tmp[i];
-		if (tmp[i] == '' || tmp[i] == '\n' || tmp[i] == 9)
-		{
-			res_i = 0;
-			res_j++;
-		}
-		res_i++;
-	}
+	result = NULL;
+
+
 	return (result);
 }
 
 int		main(void)
 {
-	int		i;
-	int		j;
-	char	*str = "Ceci est un test";
-	char	**result;
-
-	result = ft_split_whitespaces(str);
-	i = -1;
-	while (result[++i])
-		;
-	printf("%d", i);
+	char	*str = "         \n\n\n\nCeci\nest un test          a      \n\n\n\n  l";
+	printf("%d\n", get_nb_word(str));
 	return (0);
 }
