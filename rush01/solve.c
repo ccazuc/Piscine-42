31e71acd540afdc24c378cc905aa2ed5df73b7b2
Clement CAZUC <ccazuc@e3r9p11.42.fr>
2017-07-15 16:56:12 +0200

    Update rush01

--- rush01/solve.c
+++ rush01/solve.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 15:19:09 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/15 16:53:38 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/15 16:56:04 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,10 @@ int		recur_case(char **tab, char x, char y)
 		   tab[x][y] = i;
 		   increment_value(&x, &y);
 		   if (recur_case(tab, x, y) != 0)
-			   valid_result++;
+			   valid_result = recur_case(tab, x, y);
 	   }
 	decrement_value(&x, y);
 	tab[x][y] = '.';
 	increment_value(&x, &y);
-	if (recur_case(tab, x, y) != 0)
-		valid_result++;
 	return (valid_result);
 }
