bd3f489a09bfecfc49c08038e224622ec40e7d5f
Clement CAZUC <ccazuc@e3r9p11.42.fr>
2017-07-15 15:55:29 +0200

    Update rush01

--- rush01/solve.c
+++ rush01/solve.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/15 15:19:09 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/15 15:53:31 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/15 15:54:39 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ char	recur_case(char **tab, char x, char y)
 	while (++i <= '9')
 	   if (can_place(tab, x, y, i))
 	   {
-		   increment_value(&x, &y);
 		   tab[x][y] = i;
+		   increment_value(&x, &y);
 		   if (recur_case(tab, x, y) == 0)
 	   }
 	increment_value(&x, &y);
