e84d67f1b742a65f7d9dcba28e4820cc26e5fbd8
Clement CAZUC <ccazuc@e3r9p11.42.fr>
2017-07-16 16:20:25 +0200

    Start match

--- match/ex00/match.c
+++ match/ex00/match.c
@@ -6,25 +6,61 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/16 15:29:32 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/16 15:36:03 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/16 16:15:52 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int		match_until_star(char *s1, char *s2, int *i_s1, int *i_s2);
+int		match_star(char *s1, char *s2, int *i_s1, int *i_s2);
+
 int		match(char *s1, char *s2)
 {
-	
+	int		i_s1;
+	int		i_s2;
+
+	i_s1 = 0;
+	i_s2 = 0;
+	return (match_until_star(s1, s2, &i_s1, &i_s2));
 }
 
-int		match_until_star(char *s1, char *s2, int i_s1, int i_s2)
+int		match_until_star(char *s1, char *s2, int *i_s1, int *i_s2)
 {
 	int		i;
 
 	i = 0;
-	while (s1[i_s1 + i] && s2[i_s2 + i])
+	while (s1[*i_s1 + i] && s2[*i_s2 + i])
 	{
-		if (s2[i_s2 + i] != '*' && s2[i_s2 + i] != s1[i_s1 + i])
+		if (s2[*i_s2 + i] != '*' && s2[*i_s2 + i] != s1[*i_s1 + i])
 			return (0);
-		if (s2[i_s2 + i] == '*')
-			//call recurs	
+		if (s2[*i_s2 + i] == '*')
+		{
+			while (s2[*i_s2 + i] == '*')
+				++i_s2;
+			return (match_star(s1, s2, i_s1 + i, i_s2 + i));
+		}
+		++i;
 	}
+	return (s1[*i_s1 + i] == s2[*i_s2 + i]);
+}
+
+int		match_star(char *s1, char *s2, int *i_s1, int *i_s2)
+{
+	int		i;
+
+	i = 0;
+	if (!s2[*i_s2])
+		return (1);
+	while (s1[*i_s1 + i])
+	{
+		if (s1[*i_s1 + i] == s2[*i_s2])
+			return (match_until_star(s1, s2, i_s1 + i, i_s2));
+		++i;
+	}
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	printf("match: %d\n", match(argv[1], argv[2]));
+	return (0);
 }
