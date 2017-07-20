903eb2afcc00c5110a99e05818585b91ff8b7b67
Clement CAZUC <ccazuc@e3r10p13.42.fr>
2017-07-20 11:56:42 +0200

    Tail norme update

--- j12/ex02/handle_stdin.c
+++ j12/ex02/handle_stdin.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/19 14:37:35 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/19 15:06:31 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/20 11:56:30 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	read_stdin(int char_nb, int value_in_c)
 	int		i;
 
 	if (!(buffer = malloc((char_nb + 1) * sizeof(*buffer))))
-			return ;
-		while ((i = read(0, buffer, char_nb)) > 0)
-			buffer[i] = '\0';
-		ft_putstr(buffer);
+		return ;
+	while ((i = read(0, buffer, char_nb)) > 0)
+		buffer[i] = '\0';
+	ft_putstr(buffer);
 }
