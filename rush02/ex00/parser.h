043aa8485f23e6c95fb8dd9ad42a47c217247832
Clement CAZUC <ccazuc@e3r9p11.42.fr>
2017-07-22 19:28:27 +0200

    Fixed bugs in solve.c

--- rush02/ex00/parser.h
+++ rush02/ex00/parser.h
@@ -6,13 +6,17 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/22 12:22:03 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/22 12:58:51 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/22 19:28:17 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 # define BUFF_LEN 512
+# define NB_ROW 0
+# define J 1
+# define I 2
+# define CUR_ROW_LEN 3
 
 char	**parse_stdin();
 
