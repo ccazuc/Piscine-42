69e9991b6543d726d0e41773353e1cdc6b666839
Clement CAZUC <ccazuc@e3r9p11.42.fr>
2017-07-22 12:44:52 +0200

    Rush02 parse update

--- rush02/ex00/list_utils.c
+++ rush02/ex00/list_utils.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/22 12:08:15 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/22 12:15:19 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/22 12:30:14 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,35 @@ t_list	create_elem(char data)
 	return (result);
 }
 
-void	list_push_back(t_list *begin_list, char data)
+char	list_push_back(t_list **begin_list, char data)
 {
 	t_list	*result;
 	t_list	*list;
 
 	if (!begin_list)
 	{
-		begin_list = create_elem(data);
-		return ;
+		*begin_list = create_elem(data);
+		return (0);
 	}
-	result = create_elem(data);
+	if (!(result = create_elem(data)))
+		return ;
 	while (list)
 		list = list->next;
 	list->next = result;
+	return (1);
+}
+
+char	list_push_back_buffer(t_list **begin_list, char *buffer)
+{
+	int		i;
+
+	i = -1;
+	while (buffer[++i])
+	{
+		if (!begin_list)
+			begin_list = create_elem(buffer[i]);
+		else if (!(list_push_back(begin_list, buffer[i])))
+			return (0);
+	}
+	return (1);
 }
--- rush02/ex00/parser.c
+++ rush02/ex00/parser.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/22 12:05:00 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/22 12:23:20 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/22 12:35:05 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,30 @@
 #include <stdlib.h>
 #include "ft_list.h"
 
-char	**conv_linked_list(t_list *begin_list)
+int		get_nb_row(t_list *begin_list)
 {
-	
+	t_list *list;
+	int		nb_row;
+
+	list = begin_list;
+	nb_row = 0;
+	while (list)
+	{
+		if (list->data == '\n')
+			++nb_row;
+		list = list->next;
+	}
+	return (nb_row);
+}
+
+char	**conv_linked_list(t_list *begin_list, int tot_len)
+{
+	char	**result;
+	int		nb_row;
+
+	nb_row = get_nb_row(begin_list);
+	if (!(result = malloc(nb_row * sizeof(*result))))
+		return (NULL);
 }
 
 char	**parse_stdin()
@@ -24,12 +45,17 @@ char	**parse_stdin()
 	int		tot_len;
 	int		data_read;
 	char	*buffer;
+	t_list	*list;
 
 	if (!(buffer = malloc(BUFF_LEN * sizeof(*buffer))))
 		return (NULL);
 	tot_len = 0;
-	while ((data_red = read(0, buffer, BUFF_LEN - 1)) > 0)
+	list = NULL;
+	while ((data_read = read(0, buffer, BUFF_LEN - 1)) > 0)
 	{
-		
-	}	
+		tot_len =+ data_read;
+		buffer[data_read] = '\0';
+		ft_list_push_back_buffer(&list, buffer);
+	}
+	return conv_linked_list(list, tot_len);
 }
