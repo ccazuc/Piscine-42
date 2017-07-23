ec5d6144b55f5ec8065996e142ff5ab19bc68362
Clement CAZUC <ccazuc@e3r9p11.42.fr>
2017-07-23 18:13:51 +0200

    Upate

--- j11/ex05/ft_list_push_params.c
+++ j11/ex05/ft_list_push_params.c
@@ -6,7 +6,7 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 18:55:31 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/21 13:10:30 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/23 16:20:20 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,19 @@ t_list	*ft_list_push_params(int ac, char **av)
 	t_list	*result;
 	int		i;
 
-	if (ac == 0)
+	if (ac <= 1)
 		return (NULL);
-	result = ft_create_elem(av[0]);
 	i = 0;
-	while (av[++i])
+	while (++i < ac)
 	{
 		tmp = result;
 		result = ft_create_elem(av[i]);
 		result->next = tmp;
 	}
-	return (result); 
+	i = 0;
+	tmp = result;
+	while (++i < ac - 1)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	return (result);
 }
--- j11/ex06/ft_list_clear.c
+++ j11/ex06/ft_list_clear.c
@@ -6,24 +6,25 @@
 /*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/21 13:11:36 by ccazuc            #+#    #+#             */
-/*   Updated: 2017/07/21 13:13:20 by ccazuc           ###   ########.fr       */
+/*   Updated: 2017/07/23 16:23:30 by ccazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
 void	ft_list_clear(t_list **begin_list)
 {
 	t_list	*list;
-	t_list	*tmp;
+	t_list	*next;
 
 	list = *begin_list;
 	while (list)
 	{
-		tmp = list;
-		list = list->next;
-		tmp->next = NULL;
-		free(tmp);
+		next = list->next;
+		list->next = NULL;
+		free(list);
+		list = next;
 	}
 	begin_list = NULL;
 }
